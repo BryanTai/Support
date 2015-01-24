b517f11f85dc32a97af85f5eaf3b14d6f79f1bc5
dan-l <daniel9618@gmail.com>
2015-01-24 11:29:03 -0800

    Jump only only once

--- Assets/Scripts/WizardController.cs
+++ Assets/Scripts/WizardController.cs
@@ -4,16 +4,31 @@ using System.Collections;
 public class WizardController : MonoBehaviour {
 
 	public float speed;
+	public float jumpHeight;
+	public bool isJumping = false;
 
 	// Update is called once per frame
 	void Update () {
-		// Keyboard: 
+		// Keyboard horizontal movement
 		var translationH = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
-		var translationV = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
-	
 		transform.Translate (translationH, 0, 0);
-		transform.Translate (0, translationV, 0);
+
+		// Keyboard jump movement
+		if (Input.GetKeyDown (KeyCode.W)) {
+			if(isJumping == false) {
+				// only jump if we are on the ground
+				rigidbody2D.AddForce (Vector2.up * jumpHeight);
+				isJumping = true;
+			}
+		}
 	}
 
-
+	void OnCollisionEnter2D(Collision2D col) {
+		//http://www.youtube.com/watch?v=t1GB1dl_aj0
+		if (col.gameObject.tag == "Ground") {
+			// when we collide with ground 
+			isJumping = false;
+		}
+	}
+	
 }
