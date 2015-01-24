dd1f56d006b31a64050b8729ef1d5a2183c755b0
Aor_N <scc385@gmail.com>
2015-01-24 09:53:20 -0800

    add horizontal movement

--- Assets/Scripts/WizardController.cs
+++ Assets/Scripts/WizardController.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class WizardController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	public float speed;
+
 	// Update is called once per frame
 	void Update () {
+		var translation = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		transform.Translate (translation, 0, 0);
 	
 	}
 }
