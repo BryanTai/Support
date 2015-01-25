d5ef66692c67825c7103a29ab3a44b86833f9fba
Paul-Kijtapart <scc385@gmail.com>
2015-01-25 00:19:59 -0800

    wut

---
+++ Assets/Scripts/ScrollingBG.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingBG : MonoBehaviour {
+	public float scrollspeed=3;
+	public float tileSizeZ=70;
+
+	private Vector3 startPosition;
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newPosition = Mathf.Repeat (Time.time * scrollspeed, tileSizeZ);
+		transform.position = startPosition + Vector3.left * newPosition;
+	}
+}
